ec77ec784a60459111fd64a2029a0c9838c8e937
Dammy-dev66 <damilolajoshua476@gmail.com>
2025-09-11 17:02:03 +0100

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -1,4 +1,4 @@
-import { Client, Environment } from 'square';
+import { Client, Environment } from '@square/square';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
